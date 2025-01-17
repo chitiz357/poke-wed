6ed17f94b36ebeb25431d6cabe52558daf02f498
Bryan Then <thenhernandezb@gmail.com>
2025-01-16 21:08:45 -0400

    feat: functional page pokemonList

--- src/pokemonListPage/hook.pokemonList.ts
+++ src/pokemonListPage/hook.pokemonList.ts
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { getPokemonDataList } from "./services.pokemonList";
 import { emptyPokemonData } from "../hooks";
 
-export const usePokemonList = () => {
+export const usePokemonList = (page: number) => {
 	const [pokemonDataList, setPokemonDataList] = useState([emptyPokemonData]);
 
 	useEffect(() => {
-		getPokemonDataList().then((list) => setPokemonDataList(list));
-	}, []);
+		getPokemonDataList(page).then((list) => setPokemonDataList(list));
+	}, [pokemonDataList]);
 
 	return pokemonDataList;
 };
--- src/pokemonListPage/page.pokemonList.tsx
+++ src/pokemonListPage/page.pokemonList.tsx
@@ -1,12 +1,14 @@
 import { usePokemonList } from "./hook.pokemonList";
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 
 export function PokemonListPage() {
-	const list = usePokemonList();
+	const { page } = useParams();
+	const list = usePokemonList(Number(page));
 
 	return (
 		<div>
 			<h1>list</h1>
+
 			<div className="list-warper">
 				{list.map((pokemon) => (
 					<div className="card">
@@ -16,6 +18,10 @@ export function PokemonListPage() {
 						</Link>
 					</div>
 				))}
+			</div>
+			<div>
+				<Link to={`/list/${Number(page) - 1}`}>prev</Link>
+				<Link to={`/list/${Number(page) + 1}`}>next</Link>
 			</div>
 		</div>
 	);
